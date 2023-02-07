f30b48a4ded8e5dce3fee690321f67011ddcff0b
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-02-07 15:12:14 +0300

    add a listener to the EditText widget

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -1,14 +1,19 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
@@ -20,6 +25,28 @@ class CrimeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        titleField = view.findViewById(R.id.crime_title) as EditText
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val titleWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(sequence: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(sequence: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
     }
 }
